4aff2e070533829c7c5deaf538ec0188c26c15c1
michael <michael@dadamotion.be>
2021-01-26 13:09:55 +0300

    added in app id

--- cms/config/app.php
+++ cms/config/app.php
@@ -20,10 +20,11 @@
 use craft\helpers\App;
 
 return [
+    'id' => App::env('APP_ID') ?: 'CraftCMS',
     'modules' => [
         'site-module' => [
             'class' => \modules\sitemodule\SiteModule::class,
-        ],
+        ]
     ],
     'bootstrap' => ['site-module'],
     'components' => [
@@ -39,8 +40,9 @@ return [
             'throwExceptions' => App::env('DEV_MODE'),
         ],
         'queue' => [
-            'class' => craft\queue\Queue::class,
-            'ttr' => 10 * 60,
+            'class' => yii\queue\redis\Queue::class,
+            'redis' => 'redis',
+            'channel' => 'queue',
         ],
         'redis' => [
             'class' => yii\redis\Connection::class,
