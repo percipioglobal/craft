09bf6ce6e3887bea319c0b68f7e8017c19174209
Stefanie Gevaert <>
2021-10-10 13:36:42 +0200

    updated components to match vue 3

--- src/vue/molecules/notifications/notification--cookie.vue
+++ src/vue/molecules/notifications/notification--cookie.vue
@@ -1,70 +1,74 @@
 <template>
-  <div
-    v-if="showCookie"
-    :class="[
-      'fixed z-10 right-0 bottom-0 m-3 p-5 w-11/12 md:w-9/12 lg:w-4/12 text-white-100 bg-blue-600'
-    ]"
-  >
-    <span class="text-2xl font-extrabold block text-white-100 pb-3">
-      {{ options.title }}
-    </span>
-
-    <div class="text-white-100" v-html="options.article"></div>
-
-    <div class="w-full flex items-center justify-end mt-5">
-      <a
-        :href="options.url"
-        class="text-white-100 underline mr-5"
-        :title="options.privacy"
-      >
-        {{ options.privacy }}
-      </a>
-      <button
-        @click.prevent="acceptCookie"
+    <div
+        v-if="showCookie"
         :class="[
-          'inline-flex border-2 px-4 py-2 font-extrabold text-lg border-white-100  hover:bg-white-100',
-          'hover:text-' + options.colour,
+            'fixed z-10 right-0 bottom-0 m-3 p-5 w-11/12 md:w-9/12 lg:w-4/12 text-white-100 bg-blue-600'
         ]"
-        :title="options.accept"
-      >
-        {{ options.accept }}
-      </button>
+    >
+        <span class="text-2xl font-extrabold block text-white-100 pb-3">
+            {{ options.title }}
+        </span>
+
+        <div class="text-white-100" v-html="options.article"></div>
+
+        <div class="w-full flex items-center justify-end mt-5">
+            <a
+            :href="options.url"
+            class="text-white-100 underline mr-5"
+            :title="options.privacy"
+            >
+                {{ options.privacy }}
+            </a>
+
+            <button
+                @click.prevent="acceptCookie"
+                :class="[
+                    'inline-flex border-2 px-4 py-2 font-extrabold text-lg border-white-100  hover:bg-white-100',
+                    'hover:text-' + options.colour,
+                ]"
+                :title="options.accept"
+            >
+                {{ options.accept }}
+            </button>
+
+        </div>
+
     </div>
-  </div>
 </template>
 
-<script>
-  export default {
+<script lang="ts">
+    import { defineAsyncComponent, defineComponent } from 'vue'
 
-    props: {
-      options: {
-        type: Object,
-        required: true,
-      }
-    },
+    export default defineComponent({
+        props: {
+            options: {
+            type: Object,
+            required: true,
+            }
+        },
 
-    data: () => ({
-      showCookie: false,
-    }),
+        data: () => ({
+            showCookie: false,
+        }),
 
-    methods: {
+        methods: {
 
-      acceptCookie() {
-          const expiry = 5 * 24 * 60 * 60 * 1000;
-          localStorage.setItem('cookie:show', expiry);
-          this.setCookie();
-      },
+            acceptCookie() {
+                const expiry = 5 * 24 * 60 * 60 * 1000;
+                localStorage.setItem('cookie:show', expiry);
+                this.setCookie();
+            },
 
-      setCookie() {
-          this.showCookie = localStorage.getItem('cookie:show') ? false : true;
-      }
+            setCookie() {
+                this.showCookie = localStorage.getItem('cookie:show') ? false : true;
+            }
 
-    },
+        },
 
-    created() {
+        created() {
 
-        this.setCookie();
+            this.setCookie();
 
-    }
-  };
+        }
+    })
 </script>
